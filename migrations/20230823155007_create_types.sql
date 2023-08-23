b1704de8d2fbfefded30b2265feb24d4f5202f40
Matthias Nunes <matthiasnunes@gmail.com>
2023-08-23 15:59:20 -0300

    Creates types table

---
+++ migrations/20230823155007_create_types.sql
@@ -0,0 +1,14 @@
+-- +goose Up
+-- +goose StatementBegin
+create table if not exists types (
+    id serial,
+    identifier varchar(100) not null,
+    generation_id int not null,
+    damage_class_id int
+);
+-- +goose StatementEnd
+
+-- +goose Down
+-- +goose StatementBegin
+drop table types;
+-- +goose StatementEnd
