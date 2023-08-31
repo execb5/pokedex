84dddf614dcaa17f50ef8b623fa7f013f18ce195
Matthias Nunes <matthiasnunes@gmail.com>
2023-08-31 14:18:39 -0300

    Implements basic http server

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,7 +1,39 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+)
+
+type Response struct {
+	Message string `json:"message"`
+}
 
 func main() {
-	fmt.Println("vim-go")
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		response := Response{
+			Message: "Hello. I'm the Pokedex server",
+		}
+
+		jsonData, err := json.Marshal(response)
+		if err != nil {
+			http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+			return
+		}
+
+		w.Header().Set("Content-Type", "application/json")
+
+		_, err = w.Write(jsonData)
+		if err != nil {
+			http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+			return
+		}
+	})
+
+	port := 8080
+	err := http.ListenAndServe(fmt.Sprintf(":%d", port), nil)
+	if err != nil {
+		fmt.Println(err)
+	}
 }
