8938ee6405eb6232b1db8fdffed124f160f59efc
Matthias Nunes <matthiasnunes@gmail.com>
2023-09-06 11:52:43 -0300

    Gracefully shuts down server

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,8 +1,15 @@
 package main
 
 import (
+	"context"
+	"errors"
 	"fmt"
+	"log"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
 	"github.com/execb5/pokedex/pkg/router"
 )
@@ -13,8 +20,24 @@ func main() {
 	}
 
 	port := 8080
-	err := http.ListenAndServe(fmt.Sprintf(":%d", port), nil)
-	if err != nil {
-		fmt.Println(err)
+	server := &http.Server{Addr: fmt.Sprintf(":%d", port)}
+
+	go func() {
+		if err := http.ListenAndServe(fmt.Sprintf(":%d", port), nil); !errors.Is(err, http.ErrServerClosed) {
+			log.Fatalf("HTTP server error: %v", err)
+		}
+		log.Println("Stopped serving new connections")
+	}()
+
+	sc := make(chan os.Signal, 1)
+	signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM)
+	<-sc
+
+	ctx, shutdownRelease := context.WithTimeout(context.Background(), 10*time.Second)
+	defer shutdownRelease()
+
+	if err := server.Shutdown(ctx); err != nil {
+		log.Fatalf("HTTP shutdown error: %v", err)
 	}
+	log.Println("Graceful shutdown complete")
 }
