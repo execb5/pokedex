21fe497c505ff91105dbfb39cdab45fa4bdc30a5
Matthias Nunes <matthiasnunes@gmail.com>
2023-09-06 11:53:10 -0300

    Allows only GET for index and poke show routes

--- pkg/controllers/index_controller.go
+++ pkg/controllers/index_controller.go
@@ -6,6 +6,11 @@ import (
 )
 
 func Index(w http.ResponseWriter, r *http.Request) {
+	if r.Method != "GET" {
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+		return
+	}
+
 	response := Response{
 		Message: "Hello. I'm the Pokedex server",
 	}
